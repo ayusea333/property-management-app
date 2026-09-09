1504e4195724a51de8bcfb924faf2c6ec49a19f3
ayusea333 <ayusea333@gmail.com>
2026-09-09 20:36:22 +0900

    Update expenses.js

--- src/lib/expenses.js
+++ src/lib/expenses.js
@@ -13,6 +13,7 @@ export const expenseFromRow = (r) => ({
   hasReceipt: !!r.has_receipt,
   paidDate: r.paid_date || '',
   taxType: r.tax_type || '',
+  isCapitalExpenditure: !!r.is_capital_expenditure,
 })
 
 export const expenseToRow = (e) => ({
@@ -29,4 +30,5 @@ export const expenseToRow = (e) => ({
   has_receipt: !!e.hasReceipt,
   paid_date: e.paidDate || null,
   tax_type: e.taxType || null,
+  is_capital_expenditure: !!e.isCapitalExpenditure,
 })
